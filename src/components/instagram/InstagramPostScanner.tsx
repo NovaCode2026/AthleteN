163eda7a65e6ce49fa3e1bae36894244406b4aeb
Nova Code <novacode.create@gmail.com>
2026-09-13 15:43:34 +0530

    feat: add Instagram post scanner marker

---
+++ src/components/instagram/InstagramPostScanner.tsx
@@ -0,0 +1,3 @@
+// Instagram post URLs are supported by the unified Tournament Scanner.
+// This marker file is intentionally lightweight; routing remains in InstagramOrganizerScanner.tsx.
+export {};
