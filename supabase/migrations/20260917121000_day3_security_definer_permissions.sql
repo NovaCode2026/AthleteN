e093b6010d563565eccd6eb33b6befbb8c1b1e32
Nova Code <novacode.create@gmail.com>
2026-09-17 11:37:20 +0530

    Harden Day 3 trigger function permissions

---
+++ supabase/migrations/20260917121000_day3_security_definer_permissions.sql
@@ -0,0 +1,3 @@
+revoke all on function public.bootstrap_profile_from_auth() from public, anon, authenticated;
+revoke all on function public.enforce_profile_entitlement_projection() from public, anon, authenticated;
+revoke all on function public.sync_account_entitlement_to_profile() from public, anon, authenticated;
